32f60c2d0f0e81899a3f4c1f773b39a4c5bbd833
Josh <joshua@durhamgardens.co.uk>
2022-02-21 13:56:47 +0000

    Split up segment into climb/sprint

---
+++ src/cycling/IntermediateSprint.java
@@ -0,0 +1,8 @@
+package cycling;
+
+public class IntermediateSprint extends Segment {
+    // Constructor
+    IntermediateSprint(double location) {
+        super(location, SegmentType.SPRINT);
+    }
+}
--- src/cycling/Segment.java
+++ src/cycling/Segment.java
@@ -4,42 +4,14 @@ public class Segment {
     // Non-Static Attributes
     private SegmentType type;
     private double location;
-    private double avgGradient;
-    private double length;
     
     // Constuctor for sprints
-    public Segment(double location){
+    public Segment(double location, SegmentType type) {
         this.setLocation(location);
         this.setType(SegmentType.SPRINT);
-        setLength(-1.0);
-        setAvgGradient(-1.0);
-    }
-    
-    // Constructor for everything else (not Sprints)
-    public Segment(double location, SegmentType type, double gradient, double length){
-        this.setLocation(location);
-        this.setType(type);
-        setAvgGradient(gradient);
-        this.setLength(length);
     }
     
     // Getters and Setters
-    public double getLength() {
-        return length;
-    }
-
-    public void setLength(double length) {
-        this.length = length;
-    }
-
-    public double getAvgGradient() {
-        return avgGradient;
-    }
-
-    public void setAvgGradient(double avgGradient) {
-        this.avgGradient = avgGradient;
-    }
-
     public double getLocation() {
         return location;
     }
