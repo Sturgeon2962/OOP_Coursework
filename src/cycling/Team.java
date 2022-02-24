4524fb71d013076a262a14a9219ef35c82a5faad
Sturgeon2962 <tomasturgeon@gmail.com>
2022-02-24 12:50:55 +0000

    Added docstrings to Team.java

--- src/cycling/Team.java
+++ src/cycling/Team.java
@@ -1,6 +1,12 @@
 package cycling;
 import java.util.ArrayList;
 
+/**
+ * A class to create teams and allocate team members 
+ * 
+ * @author Diogo Pacheco
+ * @version 1.0
+ */
 public class Team {
     // Non-Static Team Attributes
     private String teamName;
@@ -11,7 +17,13 @@ public class Team {
     // Static Team Attributes
     public static int nextTeamId = 1;
 
-    // Constructor for a team
+    // Getter and setters
+
+    /**
+     * 
+     * @param name - The name given to the team
+     * @param description - A brief description of the team
+     */
     public Team (String name, String description){
         teamName = name;
         this.description = description;
@@ -20,30 +32,56 @@ public class Team {
     }
 
 
-    // Getters and setters for all attibutes
+    /**
+     * Getter for the team name
+     * @return - Returns the team name
+     */
     public String getTeamName(){
         return this.teamName;
     }
 
+    /**
+     * Setter for team name
+     * @param name - The name given to the team
+     */
     public void setTeamName(String name){
         this.teamName = name;
     }
 
+    /**
+     * Getter for team members
+     * @return - Returns a ArrayList of team members
+     */
     public ArrayList<Rider> getTeamMembers(){
         return teamMembers;
     }
 
+    /**
+     * Getter for team description
+     * @return - Returns a brief description of the team 
+     */
     public String getTeamDescription(){
         return this.description;
     }
 
+    /**
+     * Setter for description
+     * @param description - A brief description of the team
+     */
     public void setTeamDescription(String description){
         this.description = description;
     }
+    
+    // All other methods
 
+    /**
+     * A method to create a new rider
+     * 
+     * @param name - The name of the rider
+     * @param yearOfBirth - The year that the rider was born
+     */
     public void createRider(String name, int yearOfBirth){
         teamMembers.add(new Rider(teamId, name, yearOfBirth));
     }
 
-    // All other methods
 }
