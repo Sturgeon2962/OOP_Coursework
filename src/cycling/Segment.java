cc36ca2c5780e9293e27ed7aa6b5c0d763f53a94
Josh <joshua@durhamgardens.co.uk>
2022-02-24 12:59:06 +0000

    javadoc for segment

--- src/cycling/Segment.java
+++ src/cycling/Segment.java
@@ -1,29 +1,65 @@
 package cycling;
 
+/**
+ * This abstract class is used to represent a segment within a stage.
+ * 
+ * @author
+ * @version 1.0
+ */
 public abstract class Segment {
     // Non-Static Attributes
     private SegmentType type;
     private double location;
     
-    // Constuctor for sprints
+    /**
+     * Constructor for a Segment.
+     * 
+     * @param location The km location where the segment finished within the stage.
+     * @param type The type of segment being created - {@link SegmentType#C1},
+     * {@link SegmentType#C2}, {@link SegmentType#C3}, {@link SegmentType#C4},
+     * {@link SegmentType#HC} or {@link SegmentType#SPRINT}
+     */
     public Segment(double location, SegmentType type) {
         this.setLocation(location);
-        this.setType(SegmentType.SPRINT);
+        this.setType(type);
     }
     
-    // Getters and Setters
+    /**
+     * The method gets the location of the segment.
+     * 
+     * @return The km location where the segment finished within the stage.
+     */
     public double getLocation() {
         return location;
     }
 
+    /**
+     * The method sets the location of the segment.
+     * 
+     * @param location The km location where the segment finished within the stage.
+     */
     public void setLocation(double location) {
         this.location = location;
     }
 
+    /**
+     * The method gets the type of segment.
+     * 
+     * @return The type of the segment - {@link SegmentType#C1},
+     * {@link SegmentType#C2}, {@link SegmentType#C3}, {@link SegmentType#C4},
+     * {@link SegmentType#HC} or {@link SegmentType#SPRINT}
+     */
     public SegmentType getType() {
         return type;
     }
 
+    /**
+     * The method sets the type of segment.
+     * 
+     * @param type The type of segment being created - {@link SegmentType#C1},
+     * {@link SegmentType#C2}, {@link SegmentType#C3}, {@link SegmentType#C4},
+     * {@link SegmentType#HC} or {@link SegmentType#SPRINT}
+     */
     public void setType(SegmentType type) {
         this.type = type;
     }
