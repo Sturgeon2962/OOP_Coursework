80ebe291d87977af6017e4ad6c7eadaa38dd5ec8
Josh <joshua@durhamgardens.co.uk>
2022-03-01 15:04:31 +0000

    f

--- src/cycling/CyclingPortal.java
+++ src/cycling/CyclingPortal.java
@@ -132,9 +132,6 @@ public class CyclingPortal implements CyclingPortalInterface {
 				continue;
 			}
 		}
-		if(stage == null) {
-			throw new IDNotRecognisedException("stage ID not recognised");
-		}
 		return stage.getLength();
 	}
 
