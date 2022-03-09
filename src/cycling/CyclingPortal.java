36e04e8efff26fefc74b7e0f5d58f65ab8490e75
Josh <joshua@durhamgardens.co.uk>
2022-03-09 17:00:37 +0000

    changes have happened

--- src/cycling/CyclingPortal.java
+++ src/cycling/CyclingPortal.java
@@ -411,11 +411,12 @@ public class CyclingPortal implements CyclingPortalInterface {
 				ArrayList<LocalTime> riderTimes = new ArrayList<LocalTime>();
 				for (Result result : stageResult) {
 					LocalTime timeToReturn = LocalTime.parse("00:00:00");
-						Duration time = Duration.between(result.getRiderTimes()[0], result.getRiderTimes()[result.getRiderTimes().length-1]);
-						timeToReturn = (LocalTime) time.addTo(timeToReturn);
-						riderTimes.add(timeToReturn);
+					Duration time = Duration.between(result.getRiderTimes()[0], result.getRiderTimes()[result.getRiderTimes().length-1]);
+					timeToReturn = (LocalTime) time.addTo(timeToReturn);
+					riderTimes.add(timeToReturn);
+					System.out.println(timeToReturn);
 				}
-				while ((index > 0)&&(Duration.between(riderTimes.get(index), riderTimes.get(index-1)).toSeconds()<1)) {
+				while ((index > 0)&&(Duration.between(riderTimes.get(index), riderTimes.get(index-1)).toMillis()<1000)) {
 					index--;
 				}
 				return riderTimes.get(index);
