3947bc73294d2c8e9d6e2fbe193115d02bb64d9e
Josh <joshua@durhamgardens.co.uk>
2022-03-15 13:52:13 +0000

    f

--- src/cycling/CyclingPortal.java
+++ src/cycling/CyclingPortal.java
@@ -790,8 +790,7 @@ public class CyclingPortal implements CyclingPortalInterface {
 		ArrayList<Integer> riderOverallRank = new ArrayList<Integer>();
 		int[] riderRanks = new int[riderOverallRank.size()];
 		totalTime.entrySet().stream().sorted(
-			(t1, t2) -> t1.getValue().compareTo(t2.getValue())
-			).forEach(t -> riderOverallRank.add(t.getKey()));
+			(t1, t2) -> t1.getValue().compareTo(t2.getValue())).forEach(t -> riderOverallRank.add(t.getKey()));
 		for (int i = 0; i < riderOverallRank.size(); i++) {
 			riderRanks[i] = riderOverallRank.get(i);
 		}
